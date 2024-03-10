35359a89e753cac8421f376f795bc617e6a017f3
AlexKomanov <akomanov88@gmail.com>
2024-03-10 20:33:28 +0200

    adding sqlite3 lesson

--- lesson_002_sqlite/001_sqlite_basics.py
+++ lesson_002_sqlite/001_sqlite_basics.py
@@ -1,2 +1,22 @@
-print("Hello")
-print("Hello from Github")
+import sqlite3
+
+# Create connection with DB
+connection_manager = sqlite3.connect("gta.db")
+
+# Create a cursor object (aka driver)
+my_cursor = connection_manager.cursor()
+
+my_cursor.execute("CREATE TABLE IF NOT EXISTS gta (release_date INTEGER, release_name TEXT, release_city TEXT)")
+
+add_values_query_string = """
+INSERT INTO gta VALUES (?,?,?)
+"""
+
+query_params = (2008, "Grand Theft Auto IV", "Liberty City")
+
+my_cursor.execute(add_values_query_string, query_params)
+
+connection_manager.commit()
+
+# Close SQLite3 connection
+my_cursor.close()
