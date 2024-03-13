5ed2aab4b381495556e933eea87ccb70c4a716a9
AlexKomanov <akomanov88@gmail.com>
2024-03-13 17:59:42 +0200

    adding sqlite3 class work

--- lesson_002_sqlite/001_sqlite_basics.py
+++ lesson_002_sqlite/001_sqlite_basics.py
@@ -12,11 +12,32 @@ add_values_query_string = """
 INSERT INTO gta VALUES (?,?,?)
 """
 
-query_params = (2008, "Grand Theft Auto IV", "Liberty City")
+# query_params = (2008, "Grand Theft Auto IV", "Liberty City")
 
-my_cursor.execute(add_values_query_string, query_params)
+release_list = [
+    (1997, "Grand Theft Auto", "state of New Guernsey"),
+    (1999, "Grand Theft Auto 2", "Anywhere, USA"),
+    (2001, "Grand Theft Auto III", "Liberty City"),
+    (2002, "Grand Theft Auto: Vice City", "Vice City"),
+    (2004, "Grand Theft Auto: San Andreas", "state of San Andreas"),
+    (2008, "Grand Theft Auto IV", "Liberty City"),
+    (2013, "Grand Theft Auto V", "Los Santos")
+]
+
+# for query in release_list:
+#     my_cursor.execute(add_values_query_string, query)
+
+# my_cursor.execute(add_values_query_string, query_params)
+
+# my_cursor.executemany(add_values_query_string, release_list)
 
 connection_manager.commit()
+
+all_data = my_cursor.execute('SELECT * FROM  gta')
+
+for row in all_data:
+    print(row)
+
 
 # Close SQLite3 connection
 my_cursor.close()
