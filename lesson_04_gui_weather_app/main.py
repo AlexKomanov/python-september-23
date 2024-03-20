e7751dfd8ceb3ad5f5b0be3dbc753456400ce3e3
AlexKomanov <akomanov88@gmail.com>
2024-03-20 19:28:00 +0200

    flet code

--- lesson_04_gui_weather_app/main.py
+++ lesson_04_gui_weather_app/main.py
@@ -1,31 +1,54 @@
 import flet as ft
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def main(page: ft.Page):
-    page.title = "Weather Application"
-    page.theme_mode = "light"
+    page.title = 'Weather App'
+    page.theme_mode = 'dark'
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    input_city = ft.TextField(label="Enter a city name", width=400)
-    weather_data = ft.Text('TEST DATA', size=20, color=ft.colors.RED_800)
+    input_city = ft.TextField(label='Enter a city name', width=400)
+    weather_data = ft.Text('', size=25, color='red')
 
-    page.update()
+    def get_info(e):
+        if len(input_city.value) < 3:
+            return
 
-    def change_theme_mode():
-        return ft.icons.SUNNY if page.theme_mode == "dark" else ft.icons.DARK_MODE
+        api_key = os.getenv("API_KEY")
+        request = f"https://api.openweathermap.org/data/2.5/weather?q={input_city.value}&appid={api_key}&units=metric"
+        data_response = requests.get(request).json()
+        print(data_response)
+        temp = data_response['main']['temp']
+        city = data_response['name']
+        weather_data.value = f"{city}: {temp}°C"
+        page.update()  # try without update()
 
-    change_theme_icon = ft.IconButton(icon=change_theme_mode())
+    def change_theme(e: ft.ControlEvent):
+        page.theme_mode = 'light' if page.theme_mode == 'dark' else 'dark'
+        print(e.name)
+        change_theme_icon.icon = get_theme_icon()
+        print(e.control)
+        page.update()
+
+    def get_theme_icon():
+        return ft.icons.SUNNY if page.theme_mode == 'dark' else ft.icons.DARK_MODE
+
+    change_theme_icon = ft.IconButton(get_theme_icon(), on_click=change_theme)
 
     page.add(
-        ft.Row([change_theme_icon, ft.Text("Get Weather Of Any City")], alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row([
+            change_theme_icon,
+            ft.Text('Weather App')
+        ], alignment=ft.MainAxisAlignment.CENTER),
         ft.Row([input_city], alignment=ft.MainAxisAlignment.CENTER),
-        ft.Row([ft.ElevatedButton(text="Fetch Info")], alignment=ft.MainAxisAlignment.CENTER),
-        ft.Row([weather_data], alignment=ft.MainAxisAlignment.CENTER)
-
+        ft.Row([weather_data], alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row([ft.ElevatedButton('Get Info', on_click=get_info)], alignment=ft.MainAxisAlignment.CENTER)
     )
 
 
 ft.app(target=main)
-
-API_KEY = "6b8713d5636f326cb5cacece67d52b0f"
-request_data = 'https://api.openweathermap.org/data/2.5/weather?q={city name = input_city.value}&appid={API key}'
+# ft.app(target=main, view=ft.AppView.WEB_BROWSER)
